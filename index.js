42ec3b8d88aee45825c9c5072f1fd862e6ca786d
kaur0219 <shah0150@algonquinlive.com>
2017-06-05 19:30:41 -0400

    Update

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express')
+
+var app = express()
+
+app.get('/notes', function (req, res) {
+    //    res.json({
+    //        notes: "This is your notebook. Edit this to start saving your notes!"
+    //    })
+    res.json(investottawa.json);
+})
+
+app.listen(3000)
